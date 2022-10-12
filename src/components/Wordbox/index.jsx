ae9c994a8579e4ea69eece35c6d1b21b0741d0e8
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-10-12 13:11:53 +0200

    Added EventListener an useEffect in Worbox comp. and handleFinish callback in Stage comp.

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
-  
-  return (
-    <div className="wordbox">{lettersLeft}</div>
-  );
+const Wordbox = ({ word, onFinish }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);
+
+  const handleKeyUp = (e) => {
+    let newWord = lettersLeft.slice(1);
+    console.log(e);
+    if (e.key === lettersLeft[0]) {
+      setLettersLeft(newWord);
+    }
+    if (newWord.lenght === 0) {
+      onFinish();
+    }
+
+    useEffect(() => {
+      document.addEventListener('keyup', handleKeyUp);
+      return () => {
+        document.removeEventListener('keyup', handleKeyUp);
+      };
+    }, [lettersLeft]);
+
+    return <div className="wordbox">{lettersLeft}</div>;
+  };
 };
 
 export default Wordbox;
